c6193c32acd47dc7cba1a6a486b6e38685848953
bryanscrackgg-tech <bryanscrackgg@gmail.com>
2026-08-09 20:26:11 -0600

    Add files via upload

---
+++ CONTRASENAS.bat
@@ -0,0 +1,101 @@
+@echo off
+title Menu Contrasenas
+
+echo ---------------------------------------------------------------------
+echo --                             CONTRASENAS                         --
+echo ---------------------------------------------------------------------
+echo --                                                                 --
+echo --                        1.Roblox                                 --
+echo --                        2.Gmail                                  --
+echo --                        3.X                                      --
+echo --                        4.X                                      --
+echo --                        5.OTRAS                                  --
+echo --                        6.Exit                                   --
+echo --                                                                 --
+echo ---------------------------------------------------------------------
+echo.
+echo.
+echo Selecciona el numero de atajo opcion.
+
+set /p menu=
+
+if "%menu%"=="1" goto opcion11
+
+
+if "%menu%"=="2" goto opcion22
+
+
+if "%menu%"=="3" goto opcion33
+
+
+if "%menu%"=="4" goto opcion44
+
+
+if "%menu%"=="5" goto opcion55
+
+
+if "%menu%"=="6" goto opcion66
+
+
+if "%menu%"=="7" goto opcion77
+
+
+
+cls
+echo Opcion no valida, Presione cualquier tecla e intente nuevamente.
+pause>nul
+cls
+goto home
+
+:opcion11
+cls
+echo ROBLOX
+echo -------------------------
+echo Sherpynsito
+echo Alastor5BR
+echo -------------------------
+echo Edricse0
+echo Pikachu5ER
+echo -------------------------
+echo sherpyntu89990
+echo Alastor5BR
+echo -------------------------
+echo Sherpynsito
+echo Alastor5BR
+echo -------------------------
+
+pause>nul
+cls
+goto home
+
+:opcion22
+cls
+echo GMAIL
+echo --------------------------
+echo bryangtpz@gmail.com
+echo Alastor5BG.
+echo -------------------------- 
+echo bryanarmandogtpz@gmail.com
+echo Alastor5BG
+echo --------------------------
+echo bryanscrackgg@gmail.com
+echo Alastor5BG.
+echo -------------------------- 
+pause>nul
+cls
+goto home
+
+:opcion55
+cls
+echo META
+echo -------------------------
+echo Gmail=Bryanscrackgg@gmail.com
+echo Sherpynsito 
+echo Alastor5BMeta
+echo -------------------------
+pause>nul
+cls
+goto home
+
+:opcion66
+cls&exit
